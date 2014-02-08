e77619323d740adecdee0094a3cf491a93745606
William E. Byrd <webyrd@gmail.com>
2014-02-08 15:52:26 -0700

    added symbolo and numbero tests

--- symbolo/src/symbolo/core.clj
+++ symbolo/src/symbolo/core.clj
@@ -1,6 +1,68 @@
-(ns symbolo.core)
+(ns symbolo.core
+  (:refer-clojure :exclude [==])
+  (:require [clojure.core.logic :refer :all]
+            [clojure.core.logic.protocols :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+;; symbolo and numbero definitions courtesy of David Nolen:
+;; https://gist.github.com/swannodette/8876121
+
+(declare numbero*)
+ 
+(defn symbolo* [x]
+  (reify
+    IConstraintStep
+    (-step [this s]
+      (reify
+        clojure.lang.IFn
+        (invoke [_ s]
+          (let [x (walk s x)]
+            (when (symbol? x)
+              ((remcg this) s))))
+        IRunnable
+        (-runnable? [_]
+          (not (lvar? (walk s x))))))
+    IVerifyConstraint
+    (-verify [_ a cs]
+      (not (some (fn [c] (= (-rator c) `symbolo))
+                 (map (:cm cs) (get (:km cs) (root-var a x))))))
+    IConstraintOp
+    (-rator [_] `symbolo)
+    (-rands [_] [x])
+    IReifiableConstraint
+    (-reifyc [c v r s]
+      `(symbolo ~(-reify s x r)))
+    IConstraintWatchedStores
+    (-watched-stores [this] #{:clojure.core.logic/subst})))
+ 
+(defn symbolo [x]
+  (cgoal (symbolo* x)))
+ 
+(defn numbero* [x]
+  (reify
+    IConstraintStep
+    (-step [this s]
+      (reify
+        clojure.lang.IFn
+        (invoke [_ s]
+          (let [x (walk s x)]
+            (when (number? x)
+              ((remcg this) s))))
+        IRunnable
+        (-runnable? [_]
+          (not (lvar? (walk s x))))))
+    IVerifyConstraint
+    (-verify [_ a cs]
+      (not (some (fn [c] (= (-rator c) `numbero))
+                 (map (:cm cs) (get (:km cs) (root-var a x))))))
+    IConstraintOp
+    (-rator [_] `symbolo)
+    (-rands [_] [x])
+    IReifiableConstraint
+    (-reifyc [c v r s]
+      `(numbero ~(-reify s x r)))
+    IConstraintWatchedStores
+    (-watched-stores [this] #{:clojure.core.logic/subst})))
+ 
+(defn numbero [x]
+(cgoal (numbero* x)))
--- symbolo/test/symbolo/core_test.clj
+++ symbolo/test/symbolo/core_test.clj
@@ -1,7 +1,286 @@
 (ns symbolo.core-test
+  (:refer-clojure :exclude [==])
+  (:require [clojure.core.logic :refer :all :exclude [is]])
   (:use clojure.test
         symbolo.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest symbolo-numbero-tests
+  (testing ""
+    
+    (is (=
+         (run* [q] (symbolo q) (numbero q))
+         '()))
+
+    (is (=
+         (run* [q] (numbero q) (symbolo q))
+         '()))
+
+    (is (=
+         (run* [q]
+           (fresh [x]
+             (numbero x)
+             (symbolo x)))
+         '()))
+
+    (is (=
+         (run* [q]
+           (fresh [x]
+             (symbolo x)
+             (numbero x)))
+         '()))
+
+    (is (=
+         (run* [q]
+           (numbero q)
+           (fresh [x]
+             (symbolo x)
+             (== x q)))
+         '()))
+
+    (is (=
+         (run* [q]
+           (symbolo q)
+           (fresh [x]
+             (numbero x)
+             (== x q)))
+         '()))
+
+    (is (=
+         (run* [q]
+           (fresh [x]
+             (numbero x)
+             (== x q))
+           (symbolo q))
+         '()))
+
+    (is (=
+         (run* [q]
+           (fresh [x]
+             (symbolo x)
+             (== x q))
+           (numbero q))
+         '()))
+
+    (is (=
+         (run* [q]
+           (fresh [x]
+             (== x q)
+             (symbolo x))
+           (numbero q))
+         '()))
+
+    (is (=
+         (run* [q]
+           (fresh [x]
+             (== x q)
+             (numbero x))
+           (symbolo q))
+         '()))
+
+    (is (=
+         (run* [q]
+           (symbolo q)
+           (fresh [x]
+             (numbero x)))
+         '((_.0 (sym _.0)))))
+
+    (is (=
+         (run* [q]
+           (numbero q)
+           (fresh [x]
+             (symbolo x)))
+         '((_.0 (num _.0)))))
+
+    (is (=
+         (run* [q] 
+           (fresh [x y]
+             (symbolo x)
+             (== (list x y) q)))
+         '(((_.0 _.1) (sym _.0)))))
+
+    (is (=
+         (run* [q]    
+           (fresh [x y]
+             (numbero x)
+             (== (list x y) q)))
+         '(((_.0 _.1) (num _.0)))))
+
+    (is (=
+         (run* [q]    
+           (fresh [x y]
+             (numbero x)
+             (symbolo y)
+             (== (list x y) q)))
+         '(((_.0 _.1) (num _.0) (sym _.1)))))
+
+    (is (=
+         (run* [q]    
+           (fresh [x y]
+             (numbero x)
+             (== (list x y) q)
+             (symbolo y)))
+         '(((_.0 _.1) (num _.0) (sym _.1)))))
+
+    (is (=
+         (run* [q]    
+           (fresh [x y]
+             (== (list x y) q)
+             (numbero x)
+             (symbolo y)))
+         '(((_.0 _.1) (num _.0) (sym _.1)))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (== (list x y) q)
+             (numbero x)
+             (symbolo y))
+           (fresh [w z]
+             (== (list w z) q)))
+         '(((_.0 _.1) (num _.0) (sym _.1)))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (== (list x y) q)
+             (numbero x)
+             (symbolo y))
+           (fresh [w z]
+             (== (list w z) q)
+             (== w 5)))
+         '(((5 _.0) (sym _.0)))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (== (list x y) q)
+             (numbero x)
+             (symbolo y))
+           (fresh [w z]
+             (== 'a z)
+             (== (list w z) q)))
+         '(((_.0 a) (num _.0)))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (== (list x y) q)
+             (numbero x)
+             (symbolo y))
+           (fresh [w z]
+             (== (list w z) q)
+             (== 'a z)))
+         '(((_.0 a) (num _.0)))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (== (list x y) q)
+             (!= `(5 a) q)))
+         '(((_.0 _.1) (!= ((_.0 5) (_.1 a)))))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (== (list x y) q)
+             (!= '(5 a) q)
+             (symbolo x)))
+         '(((_.0 _.1) (sym _.0)))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (== (list x y) q)
+             (symbolo x)
+             (!= '(5 a) q)))
+         '(((_.0 _.1) (sym _.0)))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (symbolo x)
+             (== (list x y) q)
+             (!= '(5 a) q)))
+         '(((_.0 _.1) (sym _.0)))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (!= '(5 a) q)
+             (symbolo x)
+             (== (list x y) q)))
+         '(((_.0 _.1) (sym _.0)))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (!= '(5 a) q)
+             (== (list x y) q)
+             (symbolo x)))
+         '(((_.0 _.1) (sym _.0)))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (== (list x y) q)
+             (!= '(5 a) q)
+             (numbero y)))
+         '(((_.0 _.1) (num _.1)))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (== (list x y) q)
+             (numbero y)
+             (!= '(5 a) q)))
+         '(((_.0 _.1) (num _.1)))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (numbero y)
+             (== (list x y) q)
+             (!= '(5 a) q)))
+         '(((_.0 _.1) (num _.1)))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (!= '(5 a) q)
+             (numbero y)
+             (== (list x y) q)))
+         '(((_.0 _.1) (num _.1)))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (!= '(5 a) q)
+             (== (list x y) q)
+             (numbero y)))
+         '(((_.0 _.1) (num _.1)))))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (!= (list x y) q)
+             (numbero x)
+             (symbolo y)))
+         '(_.0)))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (numbero x)
+             (!= (list x y) q)
+             (symbolo y)))
+         '(_.0)))
+
+    (is (=
+         (run* [q]
+           (fresh [x y]
+             (numbero x)
+             (symbolo y)
+             (!= (list x y) q)))
+         '(_.0)))
+    
+    ))
